fd03358f0c437942ab319bd2b5fb1a3d19b5eac8
Steven Noonan <steven@uplinklabs.net>
2010-12-18 19:16:47 -0800

    switch from globals to a state struct
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- main.c
+++ main.c
@@ -8,31 +8,35 @@
 #include <stdlib.h>
 #include <string.h>
 
-uint32_t stdmax = 0;
-uint32_t extmax = 0;
+typedef struct
+{
+	uint32_t stdmax;
+	uint32_t extmax;
+	cpu_signature_t sig;
+	cpu_vendor_t vendor;
+	char procname[48];
+} cpuid_state_t;
 
-cpu_signature_t sig;
-cpu_vendor_t vendor = VENDOR_UNKNOWN;
-char procname[48];
+#define INIT_CPUID_STATE(x) { memset((x), 0, sizeof(cpuid_state_t)); }
 
 /* Makes a lot of calls easier to do. */
 static inline BOOL cpuid_native(cpu_regs_t *regs) { return cpuid(&regs->eax, &regs->ebx, &regs->ecx, &regs->edx); }
 
-typedef void(*cpu_std_handler)(cpu_regs_t *);
+typedef void(*cpu_std_handler)(cpu_regs_t *, cpuid_state_t *);
 
-void handle_std_base(cpu_regs_t *regs);
-void handle_std_features(cpu_regs_t *regs);
-void handle_std_cache(cpu_regs_t *regs);
-void handle_std_x2apic(cpu_regs_t *regs);
+void handle_std_base(cpu_regs_t *regs, cpuid_state_t *state);
+void handle_std_features(cpu_regs_t *regs, cpuid_state_t *state);
+void handle_std_cache(cpu_regs_t *regs, cpuid_state_t *state);
+void handle_std_x2apic(cpu_regs_t *regs, cpuid_state_t *state);
 
-void handle_ext_base(cpu_regs_t *regs);
-void handle_ext_features(cpu_regs_t *regs);
-void handle_ext_pname2(cpu_regs_t *regs);
-void handle_ext_pname3(cpu_regs_t *regs);
-void handle_ext_pname4(cpu_regs_t *regs);
+void handle_ext_base(cpu_regs_t *regs, cpuid_state_t *state);
+void handle_ext_features(cpu_regs_t *regs, cpuid_state_t *state);
+void handle_ext_pname2(cpu_regs_t *regs, cpuid_state_t *state);
+void handle_ext_pname3(cpu_regs_t *regs, cpuid_state_t *state);
+void handle_ext_pname4(cpu_regs_t *regs, cpuid_state_t *state);
 
-void handle_dump_std_04(cpu_regs_t *regs);
-void handle_dump_std_0B(cpu_regs_t *regs);
+void handle_dump_std_04(cpu_regs_t *regs, cpuid_state_t *state);
+void handle_dump_std_0B(cpu_regs_t *regs, cpuid_state_t *state);
 
 cpu_std_handler std_handlers[] = 
 {
@@ -119,7 +123,7 @@ cpu_std_handler ext_dump_handlers[] =
 uint32_t get_std_max();
 uint32_t get_ext_max();
 
-void dump_cpuid()
+void dump_cpuid(cpuid_state_t *state)
 {
 	uint32_t i;
 	cpu_regs_t cr_tmp;
@@ -128,37 +132,38 @@ void dump_cpuid()
 	   extmax before we can do a full dump */
 	ZERO_REGS(&cr_tmp);
 	cpuid_native(&cr_tmp);
-	std_handlers[0](&cr_tmp);
+	std_handlers[0](&cr_tmp, state);
 
 	ZERO_REGS(&cr_tmp);
 	cr_tmp.eax = 0x80000000;
 	cpuid_native(&cr_tmp);
-	ext_handlers[0](&cr_tmp);
+	ext_handlers[0](&cr_tmp, state);
 
-	for (i = 0; i <= stdmax; i++) {
+	for (i = 0; i <= state->stdmax; i++) {
 		ZERO_REGS(&cr_tmp);
 		cr_tmp.eax = i;
 		cpuid_native(&cr_tmp);
 		if (HAS_HANDLER(std_dump_handlers, i))
-			std_dump_handlers[i](&cr_tmp);
+			std_dump_handlers[i](&cr_tmp, state);
 		else
 			printf("CPUID %08x, results = %08x %08x %08x %08x | %s\n",
 				i, cr_tmp.eax, cr_tmp.ebx, cr_tmp.ecx, cr_tmp.edx, reg_to_str(&cr_tmp));
 	}
 	
-	for (i = 0x80000000; i <= extmax; i++) {
+	for (i = 0x80000000; i <= state->extmax; i++) {
 		ZERO_REGS(&cr_tmp);
 		cr_tmp.eax = i;
 		cpuid_native(&cr_tmp);
 		if (HAS_HANDLER(ext_dump_handlers, i - 0x80000000))
-			ext_dump_handlers[i - 0x80000000](&cr_tmp);
+			ext_dump_handlers[i - 0x80000000](&cr_tmp, state);
 		else
 			printf("CPUID %08x, results = %08x %08x %08x %08x | %s\n",
 				i, cr_tmp.eax, cr_tmp.ebx, cr_tmp.ecx, cr_tmp.edx, reg_to_str(&cr_tmp));
 	}
+	printf("\n");
 }
 
-void run_cpuid()
+void run_cpuid(cpuid_state_t *state)
 {
 	uint32_t i;
 	cpu_regs_t cr_tmp;
@@ -167,74 +172,74 @@ void run_cpuid()
 	   extmax before we can do a full dump */
 	ZERO_REGS(&cr_tmp);
 	cpuid_native(&cr_tmp);
-	std_handlers[0](&cr_tmp);
+	std_handlers[0](&cr_tmp, state);
 
 	ZERO_REGS(&cr_tmp);
 	cr_tmp.eax = 0x80000000;
 	cpuid_native(&cr_tmp);
-	ext_handlers[0](&cr_tmp);
+	ext_handlers[0](&cr_tmp, state);
 
-	for (i = 0; i <= stdmax; i++) {
+	for (i = 0; i <= state->stdmax; i++) {
 		ZERO_REGS(&cr_tmp);
 		cr_tmp.eax = i;
 		cpuid_native(&cr_tmp);
 		if (HAS_HANDLER(std_handlers, i))
-			std_handlers[i](&cr_tmp);
+			std_handlers[i](&cr_tmp, state);
 	}
 	
-	for (i = 0x80000000; i <= extmax; i++) {
+	for (i = 0x80000000; i <= state->extmax; i++) {
 		ZERO_REGS(&cr_tmp);
 		cr_tmp.eax = i;
 		cpuid_native(&cr_tmp);
 		if (HAS_HANDLER(ext_handlers, i - 0x80000000))
-			ext_handlers[i - 0x80000000](&cr_tmp);
+			ext_handlers[i - 0x80000000](&cr_tmp, state);
 	}
 }
 
 /* EAX = 0000 0000 */
-void handle_std_base(cpu_regs_t *regs)
+void handle_std_base(cpu_regs_t *regs, cpuid_state_t *state)
 {
 	char buf[13];
-	stdmax = regs->eax;
+	state->stdmax = regs->eax;
 	memcpy(&buf[0], &regs->ebx, sizeof(uint32_t));
 	memcpy(&buf[4], &regs->edx, sizeof(uint32_t));
 	memcpy(&buf[8], &regs->ecx, sizeof(uint32_t));
 	buf[12] = 0;
 	if (strcmp(buf, "GenuineIntel") == 0)
-		vendor = VENDOR_INTEL;
+		state->vendor = VENDOR_INTEL;
 	else if (strcmp(buf, "AuthenticAMD") == 0)
-		vendor = VENDOR_AMD;
+		state->vendor = VENDOR_AMD;
 	else
-		vendor = VENDOR_UNKNOWN;
+		state->vendor = VENDOR_UNKNOWN;
 }
 
 /* EAX = 0000 0001 */
-void handle_std_features(cpu_regs_t *regs)
+void handle_std_features(cpu_regs_t *regs, cpuid_state_t *state)
 {
-	*(uint32_t *)(&sig) = regs->eax;
+	*(uint32_t *)(&state->sig) = regs->eax;
 	printf("Family: %d\nModel: %d\nStepping: %d\n\n",
-		sig.family + sig.extfamily, sig.model + (sig.extmodel << 4), sig.stepping);
-	print_features(regs, 0x00000001, vendor);
+		state->sig.family + state->sig.extfamily, state->sig.model + (state->sig.extmodel << 4), state->sig.stepping);
+	print_features(regs, 0x00000001, state->vendor);
 	printf("\n");
 }
 
 /* EAX = 0000 0002 */
-void handle_std_cache(cpu_regs_t *regs)
+void handle_std_cache(cpu_regs_t *regs, cpuid_state_t *state)
 {
 	uint8_t i, m = regs->eax & 0xFF;
 	printf("Cache descriptors:\n");
-	print_caches(regs, &sig);
+	print_caches(regs, &state->sig);
 	for (i = 1; i < m; i++) {
 		ZERO_REGS(regs);
 		regs->eax = 2;
 		cpuid_native(regs);
-		print_caches(regs, &sig);
+		print_caches(regs, &state->sig);
 	}
 	printf("\n");
 }
 
 /* EAX = 0000 0004 */
-void handle_dump_std_04(cpu_regs_t *regs)
+void handle_dump_std_04(cpu_regs_t *regs, cpuid_state_t *state)
 {
 	uint32_t i = 0;
 	while (1) {
@@ -264,7 +269,7 @@ static const char *x2apic_level_type(uint8_t type)
 }
 
 /* EAX = 0000 000B */
-void handle_std_x2apic(cpu_regs_t *regs)
+void handle_std_x2apic(cpu_regs_t *regs, cpuid_state_t *state)
 {
 	uint32_t i = 0;
 	printf("Processor Topology:\n");
@@ -279,11 +284,10 @@ void handle_std_x2apic(cpu_regs_t *regs)
 			break;
 		i++;
 	}
-	printf("\n");
 }
 
 /* EAX = 0000 000B */
-void handle_dump_std_0B(cpu_regs_t *regs)
+void handle_dump_std_0B(cpu_regs_t *regs, cpuid_state_t *state)
 {
 	uint32_t i = 0;
 	while (1) {
@@ -300,53 +304,55 @@ void handle_dump_std_0B(cpu_regs_t *regs)
 }
 
 /* EAX = 8000 0000 */
-void handle_ext_base(cpu_regs_t *regs)
+void handle_ext_base(cpu_regs_t *regs, cpuid_state_t *state)
 {
-	extmax = regs->eax;
+	state->extmax = regs->eax;
 }
 
 /* EAX = 0000 0001 */
-void handle_ext_features(cpu_regs_t *regs)
+void handle_ext_features(cpu_regs_t *regs, cpuid_state_t *state)
 {
-	print_features(regs, 0x80000001, vendor);
+	print_features(regs, 0x80000001, state->vendor);
 	printf("\n");
 }
 
 /* EAX = 0000 0002 */
-void handle_ext_pname2(cpu_regs_t *regs)
+void handle_ext_pname2(cpu_regs_t *regs, cpuid_state_t *state)
 {
-	memset(procname, 0, sizeof(procname));
-	*(uint32_t *)&procname[0] = regs->eax;
-	*(uint32_t *)&procname[4] = regs->ebx;
-	*(uint32_t *)&procname[8] = regs->ecx;
-	*(uint32_t *)&procname[12] = regs->edx;
+	memset(state->procname, 0, sizeof(state->procname));
+	*(uint32_t *)&state->procname[0] = regs->eax;
+	*(uint32_t *)&state->procname[4] = regs->ebx;
+	*(uint32_t *)&state->procname[8] = regs->ecx;
+	*(uint32_t *)&state->procname[12] = regs->edx;
 }
 
 /* EAX = 0000 0003 */
-void handle_ext_pname3(cpu_regs_t *regs)
+void handle_ext_pname3(cpu_regs_t *regs, cpuid_state_t *state)
 {
-	*(uint32_t *)&procname[16] = regs->eax;
-	*(uint32_t *)&procname[20] = regs->ebx;
-	*(uint32_t *)&procname[24] = regs->ecx;
-	*(uint32_t *)&procname[28] = regs->edx;
+	*(uint32_t *)&state->procname[16] = regs->eax;
+	*(uint32_t *)&state->procname[20] = regs->ebx;
+	*(uint32_t *)&state->procname[24] = regs->ecx;
+	*(uint32_t *)&state->procname[28] = regs->edx;
 }
 
 /* EAX = 0000 0004 */
-void handle_ext_pname4(cpu_regs_t *regs)
+void handle_ext_pname4(cpu_regs_t *regs, cpuid_state_t *state)
 {
-	*(uint32_t *)&procname[32] = regs->eax;
-	*(uint32_t *)&procname[36] = regs->ebx;
-	*(uint32_t *)&procname[40] = regs->ecx;
-	*(uint32_t *)&procname[44] = regs->edx;
-	procname[48] = 0;
-	squeeze(procname);
-	printf("Processor Name: %s\n\n", procname);
+	*(uint32_t *)&state->procname[32] = regs->eax;
+	*(uint32_t *)&state->procname[36] = regs->ebx;
+	*(uint32_t *)&state->procname[40] = regs->ecx;
+	*(uint32_t *)&state->procname[44] = regs->edx;
+	state->procname[48] = 0;
+	squeeze(state->procname);
+	printf("Processor Name: %s\n\n", state->procname);
 }
 
 int main(unused int argc, unused char **argv)
 {
-	dump_cpuid();
-	printf("\n");
-	run_cpuid();
+	cpuid_state_t state;
+	INIT_CPUID_STATE(&state);
+	dump_cpuid(&state);
+	INIT_CPUID_STATE(&state);
+	run_cpuid(&state);
 	return 0;
 }
