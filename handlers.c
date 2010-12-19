96227d1dd3b71aff9dcf5da82985b832ad0c9c9c
Steven Noonan <steven@uplinklabs.net>
2010-12-19 00:25:21 -0800

    handlers: add AMD cache descriptor handler
    This is untested, because I don't have an AMD machine to test
    this code on. However, it should theoretically work.
    
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- handlers.c
+++ handlers.c
@@ -16,6 +16,7 @@ void handle_std_x2apic(cpu_regs_t *regs, cpuid_state_t *state);
 
 void handle_ext_base(cpu_regs_t *regs, cpuid_state_t *state);
 void handle_ext_pname(cpu_regs_t *regs, cpuid_state_t *state);
+void handle_ext_amdl1cachefeat(cpu_regs_t *regs, cpuid_state_t *state);
 void handle_ext_l2cachefeat(cpu_regs_t *regs, cpuid_state_t *state);
 
 void handle_dump_std_04(cpu_regs_t *regs, cpuid_state_t *state);
@@ -48,7 +49,7 @@ cpu_std_handler ext_handlers[] =
 	handle_ext_pname, /* 02 */
 	handle_ext_pname, /* 03 */
 	handle_ext_pname, /* 04 */
-	NULL, /* 05 */
+	handle_ext_amdl1cachefeat, /* 05 */
 	handle_ext_l2cachefeat, /* 06 */
 	NULL, /* 07 */
 	NULL, /* 08 */
@@ -263,41 +264,180 @@ void handle_ext_pname(cpu_regs_t *regs, cpuid_state_t *state)
 	}
 }
 
+static const char *amd_associativity(uint8_t assoc)
+{
+	static char buf[20];
+	switch (assoc) {
+	case 0x00:
+		return "Reserved";
+	case 0x01:
+		return "direct mapped";
+	case 0xFF:
+		return "fully associative";
+	default:
+		sprintf(buf, "%d-way associative", assoc);
+		return buf;
+	}
+}
+
+/* EAX = 8000 0005 */
+void handle_ext_amdl1cachefeat(cpu_regs_t *regs, unused cpuid_state_t *state)
+{
+	typedef struct {
+		uint8_t dtlb_assoc;
+		uint8_t dtlb_ent;
+		uint8_t itlb_assoc;
+		uint8_t itlb_ent;
+	} amd_l1_tlb_t;
+	typedef struct {
+		uint8_t size;
+		uint8_t assoc;
+		uint8_t linespertag;
+		uint8_t linesize;
+	} amd_l1_cache_t;
+	amd_l1_tlb_t *tlb;
+	amd_l1_cache_t *cache;
+
+	/* This is an AMD-only leaf. */
+	if (state->vendor != VENDOR_AMD)
+		return;
+
+	tlb = (amd_l1_tlb_t *)&regs->eax;
+	printf("L1 TLBs:\n"
+	       "  Data TLB (2MB and 4MB pages): %d entries, %s\n",
+	       tlb->dtlb_ent, amd_associativity(tlb->dtlb_assoc));
+	printf("  Instruction TLB (2MB and 4MB pages): %d entries, %s\n",
+	       tlb->itlb_ent, amd_associativity(tlb->itlb_assoc));
+
+	tlb = (amd_l1_tlb_t *)&regs->ebx;
+	printf("  Data TLB (4KB pages): %d entries, %s\n",
+	       tlb->dtlb_ent, amd_associativity(tlb->dtlb_assoc));
+	printf("  Instruction TLB (4KB pages): %d entries, %s\n",
+	       tlb->itlb_ent, amd_associativity(tlb->itlb_assoc));
+
+	cache = (amd_l1_cache_t *)&regs->ecx;
+	printf("L1 caches:\n"
+	       "  Data: %dKB, %s, %d lines per tag, %d byte line size\n",
+	       cache->size,
+	       amd_associativity(cache->assoc),
+	       cache->linespertag,
+	       cache->linesize);
+
+	cache = (amd_l1_cache_t *)&regs->edx;
+	printf("  Instruction: %dKB, %s, %d lines per tag, %d byte line size\n",
+	       cache->size,
+	       amd_associativity(cache->assoc),
+	       cache->linespertag,
+	       cache->linesize);
+}
+
 /* EAX = 8000 0006 */
 void handle_ext_l2cachefeat(cpu_regs_t *regs, unused cpuid_state_t *state)
 {
-	static const char *assoc[] = {
-		/* 0x00 */ "Disabled",
-		/* 0x01 */ "Direct mapped",
-		/* 0x02 */ "2-way",
-		/* 0x03 */ NULL,
-		/* 0x04 */ "4-way",
-		/* 0x05 */ NULL,
-		/* 0x06 */ "8-way",
-		/* 0x07 */ NULL,
-		/* 0x08 */ "16-way",
-		/* 0x09 */ NULL,
-		/* 0x0A */ NULL,
-		/* 0x0B */ NULL,
-		/* 0x0C */ NULL,
-		/* 0x0D */ NULL,
-		/* 0x0E */ NULL,
-		/* 0x0F */ "Fully associative"
-	};
+	if (state->vendor == VENDOR_INTEL) {
+		static const char *assoc[] = {
+			/* 0x00 */ "Disabled",
+			/* 0x01 */ "Direct mapped",
+			/* 0x02 */ "2-way",
+			/* 0x03 */ NULL,
+			/* 0x04 */ "4-way",
+			/* 0x05 */ NULL,
+			/* 0x06 */ "8-way",
+			/* 0x07 */ NULL,
+			/* 0x08 */ "16-way",
+			/* 0x09 */ NULL,
+			/* 0x0A */ NULL,
+			/* 0x0B */ NULL,
+			/* 0x0C */ NULL,
+			/* 0x0D */ NULL,
+			/* 0x0E */ NULL,
+			/* 0x0F */ "Fully associative"
+		};
 
-	typedef struct {
-		uint8_t linesize;
-		uint8_t reserved1:4;
-		uint8_t assoc:4;
-		uint16_t size;
-	} l2cache_feat_t;
+		typedef struct {
+			uint8_t linesize;
+			uint8_t reserved1:4;
+			uint8_t assoc:4;
+			uint16_t size;
+		} l2cache_feat_t;
 
-	l2cache_feat_t *feat = (l2cache_feat_t *)&regs->ecx;
+		l2cache_feat_t *feat = (l2cache_feat_t *)&regs->ecx;
 
-	printf("L2 cache:\n"
-	       "  %d%cB, %s associativity, %d byte line size\n\n",
-		feat->size > 1024 ? feat->size / 1024 : feat->size,
-		feat->size > 1024 ? 'M' : 'K',
-		assoc[feat->assoc] ? assoc[feat->assoc] : "Unknown",
-		feat->linesize);
+		printf("L2 cache:\n"
+		       "  %d%cB, %s associativity, %d byte line size\n\n",
+			feat->size > 1024 ? feat->size / 1024 : feat->size,
+			feat->size > 1024 ? 'M' : 'K',
+			assoc[feat->assoc] ? assoc[feat->assoc] : "Unknown",
+			feat->linesize);
+	}
+
+	if (state->vendor == VENDOR_AMD) {
+		static const char *assoc[] = {
+			/* 0x00 */ "Disabled",
+			/* 0x01 */ "Direct mapped",
+			/* 0x02 */ "2-way",
+			/* 0x03 */ NULL,
+			/* 0x04 */ "4-way",
+			/* 0x05 */ NULL,
+			/* 0x06 */ "8-way",
+			/* 0x07 */ NULL,
+			/* 0x08 */ "16-way",
+			/* 0x09 */ NULL,
+			/* 0x0A */ "32-way",
+			/* 0x0B */ "48-way",
+			/* 0x0C */ "64-way",
+			/* 0x0D */ "96-way",
+			/* 0x0E */ "128-way",
+			/* 0x0F */ "Fully associative"
+		};
+
+		typedef struct {
+			uint8_t  dtlb_assoc:4;
+			uint16_t dtlb_size:12;
+			uint8_t  itlb_assoc:4;
+			uint16_t itlb_size:12;
+		} l2_tlb_t;
+		typedef struct {
+			uint16_t size;
+			uint8_t  assoc:4;
+			uint8_t  linespertag:4;
+			uint8_t  linesize;
+		} l2_cache_t;
+		typedef struct {
+			uint16_t size:14;
+			uint8_t  reserved:2;
+			uint8_t  linespertag:4;
+			uint8_t  linesize;
+		} l3_cache_t;
+
+		l2_tlb_t *tlb;
+		l2_cache_t *l2_cache;
+		l3_cache_t *l3_cache;
+
+		tlb = (l2_tlb_t *)&regs->eax;
+		printf("L2 TLBs:\n"
+		       "  Data TLB (2MB and 4MB pages): %d entries, %s\n",
+		       tlb->dtlb_size, amd_associativity(tlb->dtlb_assoc));
+		printf("  Instruction TLB (2MB and 4MB pages): %d entries, %s\n",
+		       tlb->itlb_size, amd_associativity(tlb->itlb_assoc));
+
+		tlb = (l2_tlb_t *)&regs->ebx;
+		printf("  Data TLB (4KB pages): %d entries, %s\n",
+		       tlb->dtlb_size, amd_associativity(tlb->dtlb_assoc));
+		printf("  Instruction TLB (4KB pages): %d entries, %s\n",
+		       tlb->itlb_size, amd_associativity(tlb->itlb_assoc));
+
+		l2_cache = (l2_cache_t *)&regs->ecx;
+		printf("L2 cache: %dKB, %s, %d lines per tag, %d byte line size\n",
+		       l2_cache->size,
+		       assoc[l2_cache->assoc] ? assoc[l2_cache->assoc] : "unknown associativity",
+		       l2_cache->linespertag,
+		       l2_cache->linesize);
+
+		l3_cache = (l3_cache_t *)&regs->edx;
+		printf("L3 cache: %dKB, %d lines per tag, %d byte line size\n",
+		       l3_cache->size,
+		       l3_cache->linespertag,
+		       l3_cache->linesize);
+	}
 }
