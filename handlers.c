3280ac35037e2e517726ba477832d5e9b1873ec3
Steven Noonan <steven@uplinklabs.net>
2010-12-22 13:56:14 -0800

    handlers.c: add dump handlers for 0x0 and 0x8..0
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- handlers.c
+++ handlers.c
@@ -20,8 +20,10 @@ void handle_ext_pname(cpu_regs_t *regs, cpuid_state_t *state);
 void handle_ext_amdl1cachefeat(cpu_regs_t *regs, cpuid_state_t *state);
 void handle_ext_l2cachefeat(cpu_regs_t *regs, cpuid_state_t *state);
 
+void handle_dump_std_base(cpu_regs_t *regs, cpuid_state_t *state);
 void handle_dump_std_04(cpu_regs_t *regs, cpuid_state_t *state);
 void handle_dump_std_0B(cpu_regs_t *regs, cpuid_state_t *state);
+void handle_dump_ext_base(cpu_regs_t *regs, cpuid_state_t *state);
 
 cpu_std_handler std_handlers[] =
 {
@@ -65,7 +67,7 @@ cpu_std_handler ext_handlers[] =
 
 cpu_std_handler std_dump_handlers[] =
 {
-	handle_std_base, /* 00 */
+	handle_dump_std_base, /* 00 */
 	NULL, /* 01 */
 	NULL, /* 02 */
 	NULL, /* 03 */
@@ -85,7 +87,7 @@ cpu_std_handler std_dump_handlers[] =
 
 cpu_std_handler ext_dump_handlers[] =
 {
-	handle_ext_base, /* 00 */
+	handle_dump_ext_base, /* 00 */
 	NULL, /* 01 */
 	NULL, /* 02 */
 	NULL, /* 03 */
@@ -103,6 +105,19 @@ cpu_std_handler ext_dump_handlers[] =
 	NULL  /* 0F */
 };
 
+
+/* EAX = 0000 0000 */
+void handle_dump_std_base(cpu_regs_t *regs, cpuid_state_t *state)
+{
+	handle_std_base(regs, state);
+	printf("CPUID %08x, results = %08x %08x %08x %08x | %s\n",
+		state->last_leaf.eax,
+		regs->eax,
+		regs->ebx,
+		regs->ecx,
+		regs->edx,
+		reg_to_str(regs));
+}
 
 /* EAX = 0000 0000 */
 void handle_std_base(cpu_regs_t *regs, cpuid_state_t *state)
@@ -334,6 +349,19 @@ void handle_dump_std_0B(cpu_regs_t *regs, cpuid_state_t *state)
 			break;
 		i++;
 	}
+}
+
+/* EAX = 8000 0000 */
+void handle_dump_ext_base(cpu_regs_t *regs, cpuid_state_t *state)
+{
+	handle_ext_base(regs, state);
+	printf("CPUID %08x, results = %08x %08x %08x %08x | %s\n",
+		state->last_leaf.eax,
+		regs->eax,
+		regs->ebx,
+		regs->ecx,
+		regs->edx,
+		reg_to_str(regs));
 }
 
 /* EAX = 8000 0000 */
