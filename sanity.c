6215da28246004be46a79bb31023c07fcfbc4b54
Steven Noonan <steven@uplinklabs.net>
2011-02-13 22:07:16 -0800

    sanity.c: get more consistent results in APIC check
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- sanity.c
+++ sanity.c
@@ -4,6 +4,8 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/time.h>
+#include <unistd.h>
 
 #include "cpuid.h"
 #include "sanity.h"
@@ -21,48 +23,81 @@ sanity_handler_t handlers[] = {
 	NULL
 };
 
-static unsigned char get_apicid_for_cpu(struct cpuid_state_t *state, unsigned int index)
+static uint8_t get_apicid(struct cpuid_state_t *state)
 {
 	struct cpu_regs_t regs;
 	ZERO_REGS(&regs);
 	regs.eax = 1;
-	thread_bind(index);
 	state->cpuid_call(&regs, state);
 	regs.ebx = regs.ebx >> 24;
 	return (char)(regs.ebx & 0xFF);
 }
 
-static int apic_compare(const void *a, const void *b)
+static uint8_t get_apicid_for_cpu(struct cpuid_state_t *state, uint32_t index)
 {
-	return (*(const unsigned char *)a - *(const unsigned char *)b);
+	thread_bind(index);
+	return get_apicid(state);
 }
 
-static void *apic_nonsensical_worker_thread(void *flag)
+static void *apic_nonsensical_worker_thread(void *flagptr)
 {
-	int i, t, s = 0, hwthreads = thread_count();
-	while (*(unsigned char *)flag) {
+	uint8_t *flag = (uint8_t *)flagptr;
+	uint32_t i, t, hwthreads = thread_count();
+	struct timeval tv;
+	while (*flag) {
 		thread_bind(rand() % hwthreads);
-		t = (rand() % 4096 + 1);
-		for (i = 0; i < t && *(unsigned char *)flag; i++)
-			s = i * t;
-		srand(s);
+		t = (rand() % 81920+ 1);
+		for (i = 0; i < t && *flag; i++)
+			gettimeofday(&tv, NULL);
 	}
 	pthread_exit(NULL);
 	return NULL;
 }
 
+struct apic_validate_t {
+	struct cpuid_state_t *state;
+	uint32_t index;
+	uint8_t *worker_flag;
+	uint8_t expected;
+	uint8_t failed;
+};
+
+static void *apic_validation_thread(void *ptr)
+{
+	struct apic_validate_t *meta = (struct apic_validate_t *)ptr;
+	thread_bind(meta->index);
+	while (!meta->failed && *meta->worker_flag) {
+		usleep(5000);
+		if (get_apicid(meta->state) != meta->expected) {
+			meta->failed = 1;
+		}
+	}
+	pthread_exit(NULL);
+	return NULL;
+}
+
+static int apic_compare(const void *a, const void *b)
+{
+	return (*(const uint8_t *)a - *(const uint8_t *)b);
+}
+
 static int sane_apicid(struct cpuid_state_t *state)
 {
-	unsigned int hwthreads = thread_count(), i, j,
+	int ret = 0;
+	uint32_t hwthreads = thread_count(), i,
 	    worker_count, oldbinding;
-	unsigned char *apic_ids = NULL, *apic_copy = NULL,
-	    worker_flag;
-	pthread_t *workers = NULL;
+	uint8_t *apic_ids = NULL, *apic_copy = NULL, worker_flag;
+	struct apic_validate_t *apic_state = NULL;
+	struct timeval start, now;
+	pthread_t *busy_workers = NULL,
+	    *apic_workers = NULL;
+
+	worker_count = hwthreads / 4 + 1;
 
 	/* Store the current affinity mask. It will get clobbered. */
 	oldbinding = thread_get_binding();
 
-	printf("Verifying APIC ID sanity... ");
+	printf("Verifying APIC ID sanity");
 
 	/* Populate initial APIC ID array. */
 	apic_ids = malloc(hwthreads * sizeof(unsigned char));
@@ -77,7 +112,8 @@ static int sane_apicid(struct cpuid_state_t *state)
 	for (i = 1; i < hwthreads; i++) {
 		if (apic_ids[i - 1] == apic_ids[i]) {
 			printf("fail (duplicate APIC IDs)\n");
-			return 1;
+			ret = 1;
+			goto cleanup;
 		}
 	}
 	free(apic_copy);
@@ -85,32 +121,67 @@ static int sane_apicid(struct cpuid_state_t *state)
 	/* Spawn a few busy threads to incur thread migrations,
 	   if they're going to happen at all. */
 	worker_flag = 1;
-	worker_count = hwthreads / 2 + 1;
-	workers = malloc(worker_count * sizeof(pthread_t));
+	busy_workers = malloc(worker_count * sizeof(pthread_t));
 	for (i = 0; i < worker_count; i++)
-		pthread_create(&workers[i], NULL, apic_nonsensical_worker_thread, (void *)&worker_flag);
+		pthread_create(&busy_workers[i], NULL, apic_nonsensical_worker_thread, &worker_flag);
 
 	/* Now verify that the APIC IDs don't change over time. */
-	for (i = 0; i < 1024; i++) {
-		for (j = 0; j < hwthreads; j++) {
-			if (apic_ids[j] != get_apicid_for_cpu(state, j)) {
-				printf("fail (APIC IDs changed over time)\n");
-				return 2;
-			}
-		}
+	apic_state = malloc(hwthreads * sizeof(struct apic_validate_t));
+	apic_workers = malloc(hwthreads * sizeof(pthread_t));
+	memset(apic_state, 0, hwthreads * sizeof(struct apic_validate_t));
+	for (i = 0; i < hwthreads; i++) {
+		apic_state[i].state = state;
+		apic_state[i].index = i;
+		apic_state[i].expected = apic_ids[i];
+		apic_state[i].worker_flag = &worker_flag;
+		pthread_create(&apic_workers[i], NULL, apic_validation_thread, &apic_state[i]);
 	}
 	free(apic_ids);
 
+	/* Occasionally signal workers to run validation checks. */
+	gettimeofday(&start, NULL);
+	while(worker_flag) {
+		gettimeofday(&now, NULL);
+		if (now.tv_sec - start.tv_sec > 3)
+			break;
+		printf(".");
+		fflush(stdout);
+		usleep(250000);
+		for (i = 0; i < hwthreads; i++) {
+			if (apic_state[i].failed) {
+				printf(" failed (APIC IDs changed over time)\n");
+				ret = 2;
+				goto cleanup;
+			}
+		}
+	}
+
+	printf(" ok\n");
+
+cleanup:
 	/* Wait for workers to exit. */
 	worker_flag = 0;
-	for (i = 0; i < worker_count; i++)
-		pthread_join(workers[i], NULL);
+	if (busy_workers) {
+		for (i = 0; i < worker_count; i++) {
+			pthread_join(busy_workers[i], NULL);
+		}
+		free(busy_workers);
+	}
+
+	if (apic_workers) {
+		for (i = 0; i < hwthreads; i++) {
+			pthread_join(apic_workers[i], NULL);
+		}
+		free(apic_workers);
+	}
+	
+	if (apic_state)
+		free(apic_state);
 
 	/* Restore the affinity mask from before. */
 	thread_bind_mask(oldbinding);
 
-	printf("ok\n");
-	return 0;
+	return ret;
 }
 
 static int sane_l3_sharing(struct cpuid_state_t *state)
@@ -124,9 +195,9 @@ static int sane_l3_sharing(struct cpuid_state_t *state)
 		unsigned max_threads_sharing:12; /* +1 encoded */
 		unsigned apics_reserved:6; /* +1 encoded */
 	};
-	printf("Verifying L3 thread sharing sanity... ");
 	uint32_t i = 0;
 	struct cpu_regs_t regs;
+	printf("Verifying L3 thread sharing sanity... ");
 	while (1) {
 		struct eax_cache04_t *eax;
 		ZERO_REGS(&regs);
@@ -134,10 +205,6 @@ static int sane_l3_sharing(struct cpuid_state_t *state)
 		regs.ecx = i;
 		state->cpuid_call(&regs, state);
 
-		/* This is a non-official check. With other leafs (i.e. 0x0B),
-		   some extra information comes through, past the termination
-		   condition. I want to show all the information the CPU provides,
-		   even if it's not specified by the Intel docs. */
 		if (!(regs.eax & 0xF))
 			break;
 
