2bd27d789ca53cb78fd34c01abefd0b7dfbb3a75
Steven Noonan <steven@uplinklabs.net>
2013-09-09 10:33:26 -0700

    leaf D: print out unknown feature bits
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- handlers.c
+++ handlers.c
@@ -771,8 +771,8 @@ void handle_std_ext_state(struct cpu_regs_t *regs, struct cpuid_state_t *state)
 					continue;
 				name = xsave_feature_name(j);
 				if (!name)
-					continue;
-				printf("    %d - %s\n",j, name);
+					name = "Unknown";
+				printf("    %d - %s\n", j, name);
 			}
 			printf("\n");
 		} else if (i == 0) {
@@ -786,8 +786,8 @@ void handle_std_ext_state(struct cpu_regs_t *regs, struct cpuid_state_t *state)
 					continue;
 				name = xsave_leaf_name(j);
 				if (!name)
-					continue;
-				printf("    %d - %s\n",j, name);
+					name = "Unknown";
+				printf("    %d - %s\n", j, name);
 			}
 			printf("\n");
 
