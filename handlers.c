ca34905165ef2828f42f2d8c14db498226e8ac89
Steven Noonan <steven@uplinklabs.net>
2014-01-19 15:34:05 -0800

    MWAIT: support C0-C7 instead of just C0-C4
    This matches the September 2013 manuals.
    
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- handlers.c
+++ handlers.c
@@ -446,7 +446,7 @@ void handle_std_monitor(struct cpu_regs_t *regs, struct cpuid_state_t *state)
 	if (ecx->interrupts_as_break)
 		printf("  Interrupts as break-event for MWAIT, even when interrupts off\n");
 	if (state->vendor & VENDOR_INTEL) {
-		for (i = 0; i < 5; i++) {
+		for (i = 0; i < 8; i++) {
 			printf("  C%d sub C-states supported by MWAIT: %d\n", i, (edx->c & (0xF << (i * 4))) >> (i * 4));
 		}
 	}
