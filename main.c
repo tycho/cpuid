bead1a3f2a9b798cf0a528e750361f06d4409ea2
Steven Noonan <steven@uplinklabs.net>
2018-09-05 08:01:46 -0700

    mac os x: allow decoding all CPUs in dump files
    We had hard-coded a start/end of index 0, but this should only apply
    when we're looking at the live system rather than at a dump file.
    
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- main.c
+++ main.c
@@ -313,15 +313,16 @@ int main(int argc, char **argv)
 	}
 
 	if (cpu_start == -1) {
+		cpu_start = 0;
+		cpu_end = state.thread_count(&state) - 1;
 #ifdef TARGET_OS_MACOSX
 		/* Because thread_bind() doesn't work on Mac. Stupidest
 		 * operating system design ever.
 		 */
-		cpu_start = 0;
-		cpu_end = 0;
-#else
-		cpu_start = 0;
-		cpu_end = state.thread_count(&state) - 1;
+		if (!file) {
+			cpu_start = 0;
+			cpu_end = 0;
+		}
 #endif
 	} else {
 		cpu_end = cpu_start;
