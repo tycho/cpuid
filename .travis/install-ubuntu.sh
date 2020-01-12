a8ceb27c33ed4f05ccaa74894582a79b0d00e7f3
Steven Noonan <steven@uplinklabs.net>
2020-01-12 13:57:34 -0800

    travis: add lsb-release to Ubuntu packages
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- .travis/install-ubuntu.sh
+++ .travis/install-ubuntu.sh
@@ -7,6 +7,6 @@ apt-get update
 apt-get install -y locales
 locale-gen en_US.UTF-8
 
-PACKAGES=(git build-essential pkg-config meson clang perl)
+PACKAGES=(git build-essential pkg-config meson clang perl lsb-release)
 
 apt-get install -y "${PACKAGES[@]}"
