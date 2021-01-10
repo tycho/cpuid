c3149f306eb87125daf32be06a6f8a741292498a
Steven Noonan <steven@uplinklabs.net>
2021-01-10 09:05:28 -0800

    install: make all output more readable on GitHub Actions
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- .github/install.sh
+++ .github/install.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
-. .github/install-${IMAGE//\//-}.sh
+
+set -e
+set -o pipefail
+
+INSTALL_SCRIPT_CANDIDATES=(
+	"$PWD/.github/install-${IMAGE//\//-}.sh"
+	"$PWD/.github/install-${IMAGE%/*}.sh"
+)
+
+for INSTALL_SCRIPT in "${INSTALL_SCRIPT_CANDIDATES[@]}"; do
+	if [[ -f "${INSTALL_SCRIPT}" ]]; then
+		bash "${INSTALL_SCRIPT}" | cat
+		exit $?
+	fi
+done
+
+echo "Could not find install script in any of these paths:"
+for INSTALL_SCRIPT in "${INSTALL_SCRIPT_CANDIDATES[@]}"; do
+	echo "   ${INSTALL_SCRIPT}"
+done
+echo ""
+echo "Don't know how to install packages for ${IMAGE}!"
+echo ""
+exit 1
