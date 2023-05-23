b57bf55f14dfce6de47bd6df0cf84294bbde3ce7
Steven Noonan <steven@uplinklabs.net>
2023-05-23 13:20:46 -0700

    actions: make compatible with 'act'
    Tool from here: https://github.com/nektos/act
    
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- .github/preinstall.sh
+++ .github/preinstall.sh
@@ -2,6 +2,12 @@
 
 if [ "$IMAGE" = "alpine" ]
 then
+	if [ "x$ACT" != "x" ]
+	then
+		# I use local repository mirrors that need to use HTTP since they're
+		# set up via DNS manipulation (and thus the TLS CN doesn't match).
+		sed -ri 's/https/http/g' /etc/apk/repositories
+	fi
 	apk update
 	apk add bash
 fi
